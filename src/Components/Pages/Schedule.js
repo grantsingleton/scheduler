497528328b587515a44b5e5290699bbef41d4f4a
Grant Singleton <grantwsingleton@gmail.com>
2020-07-31 16:13:44 -0600

    Update Schedule.js

--- src/Components/Pages/Schedule.js
+++ src/Components/Pages/Schedule.js
@@ -26,12 +26,39 @@ function Schedule(props) {
         setCalName(event.target.value)
     }
 
+    const handleAppt = (event) => {
+
+    }
+
+    const handleTime = (event) => {
+
+    }
+
     const newCalForm = () => {
         return (
             <Form style={styles.form}>
-                <Form.Group controlId="formBasicEmail">
+                <Form.Group controlId="calName">
                     <Form.Label>Calendar Name</Form.Label>
                     <Form.Control onChange={handleName} type="name" placeholder="Enter name" />
+                </Form.Group>
+                <Form.Group controlId="calAppt">
+                    <Form.Label>Appointment Type</Form.Label>
+                    <Form.Control onChange={handleAppt} type="appt" placeholder="Enter appointment type" />
+                    <Form.Label>Appointment Duration</Form.Label>
+                    <Form.Control as='select' onChange={handleTime} type="appt" placeholder="Enter the amount of time for this type of appointment">
+                        <option value={15}>15 min</option>
+                        <option value={30}>30 min</option>
+                        <option value={45}>45 min</option>
+                        <option value={60}>1 hour</option>
+                        <option value={90}>1 hour 30 min</option>
+                        <option value={120}>2 hour</option>
+                        <option value={150}>2 hour 30 min</option>
+                        <option value={180}>3 hour</option>
+                        <option value={210}>3 hour 30 min</option>
+                        <option value={240}>4 hour</option>
+                        <option value={300}>5 hour</option>
+                    </Form.Control>
+                    <Button>Add another appointment</Button>
                 </Form.Group>
             </Form>
         )
